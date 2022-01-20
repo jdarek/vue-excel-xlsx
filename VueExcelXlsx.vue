c37237895cbabe4a983e2cab35ae2e70b1565c6b
Dariusz Jurek <Dariusz.Jurek@ttpsc.pl>
2022-01-20 14:36:48 +0100

    don't know

--- VueExcelXlsx.vue
+++ VueExcelXlsx.vue
@@ -48,22 +48,36 @@
                 let createXLSLFormatObj = [];
                 let newXlsHeader = [];
                 let vm = this;
+                if(typeof vm.beforeGenerate === 'function'){
+                    await vm.beforeGenerate();
+                }
                 if (vm.columns.length === 0){
                     console.log("Add columns!");
                     return;
                 }
-                if (vm.data.length === 0){
-                    console.log("Add data!");
-                    return;
+                let data = vm.data;
+                if (data.length === 0){
+                    if(typeof vm.fetch === 'function'){
+                        data = await vm.fetch();
+                    }
+                    if (vm.promisedData){
+                    	data = await vm.promisedData;
+                    }
+                    if (data.length === 0){
+                    	console.log("Add data!");
+                    	return;
+                    }
                 }
-                $.each(vm.columns, function(index, value) {
-                    newXlsHeader.push(value.label);
+
+                vm.columns.forEach( function(value, index){
+                	newXlsHeader.push(value.label);
                 });
+
                 createXLSLFormatObj.push(newXlsHeader);
-                $.each(vm.data, function(index, value) {
-                    let innerRowData = [];
-                    $.each(vm.columns, function(index, val) {
-                        if (val.dataFormat && typeof val.dataFormat === 'function') {
+                data.forEach(function(value, index){
+                	let innerRowData = [];
+                    vm.columns.forEach(function(val, index){
+                    	if (val.dataFormat && typeof val.dataFormat === 'function') {
                             innerRowData.push(val.dataFormat(value[val.field]));
                         }else {
                             innerRowData.push(value[val.field]);
@@ -71,14 +85,17 @@
                     });
                     createXLSLFormatObj.push(innerRowData);
                 });
+
                 let filename = vm.filename + ".xlsx";
+
                 let ws_name = vm.sheetname;
+
                 let wb = XLSX.utils.book_new(),
                     ws = XLSX.utils.aoa_to_sheet(createXLSLFormatObj);
                 XLSX.utils.book_append_sheet(wb, ws, ws_name);
                 // XLSX.writeFile(wb, filename);
                 let wopts = { bookType:'xlsx', bookSST:false, type:'array' };
-                let wbout = XLSX.write(wb, wopts);
+                var wbout = XLSX.write(wb, wopts);
                 saveAs(new Blob([wbout],{type:"application/octet-stream"}), filename);
             }
         }
